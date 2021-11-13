3a5e08d3b69664b813274a8aacbd216de57752c4
Fafemi Adeola <adeola5678@gmail.com>
2021-11-13 19:40:13 +0100

    updates

--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,14 +1,62 @@
-const anchor = require('@project-serum/anchor');
+// const anchor = require('@project-serum/anchor');
 
-describe('myepicproject', () => {
+// describe('myepicproject', () => {
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.Provider.env());
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.Myepicproject;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+//   it('Is initialized!', async () => {
+//     // Add your test here.
+//     const program = anchor.workspace.Myepicproject;
+//     const tx = await program.rpc.initialize();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
+
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
+
+const main = async () => {
+  console.log("🚀 Starting test...");
+
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Myepicproject;
+  const baseAccount = anchor.web3.Keypair.generate();
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
   });
-});
+  console.log("📝 Your transaction signature", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // Call add_gif!
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+
+  // Get the account again to see what changed.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
